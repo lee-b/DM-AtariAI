7bd7f6f9459a588c9ed9d1c04c7049d3a969f738
hamzeh <hamzeh@hamzeh-Inspiron-7520.(none)>
2014-06-12 11:01:44 -0400

    Actions introduced (Always right)

--- src/ale_interface.hs
+++ src/ale_interface.hs
@@ -12,7 +12,7 @@ main = do (readProcess "rm" ["ale_fifo_out"] "")
           let fromA_IO = openFd "ale_fifo_out" ReadOnly  Nothing defaultFileFlags
           putStrLn "Hangtest 1"      
           
-          -- Command 1) ../ale/ale_0.4.4/ale_0_4/ale -max_num_episodes 5 -game_controller fifo_named -disable_colour_averaging true -run_length_encoding false -frame_skip 4 -display_screen true ../ale/ale_0.4.4/ale_0_4/roms/space_invaders.bin
+          -- Command 1)  ./ale_interface > output.txt & ../ale/ale_0.4.4/ale_0_4/ale -max_num_episodes 5 -game_controller fifo_named -disable_colour_averaging true -run_length_encoding false -frame_skip 0 -display_screen true ../ale/ale_0.4.4/ale_0_4/roms/space_invaders.bin
 
           fromA <- fromA_IO
           putStrLn "Hangtest 3"
@@ -20,18 +20,25 @@ main = do (readProcess "rm" ["ale_fifo_out"] "")
           toA <- toA_IO   
           putStrLn "Hangtest 2"
 
-          (str, _len) <- fdRead fromA 64
+          (str, _len) <- fdRead fromA (160 * 210 * 2 + 200)
           putStrLn str
           putStrLn "Hangtest 4"
           
           fdWrite toA "1,0,0,1\n"
           putStrLn "Hangtest 5"
 
-          foreverPrint fromA
+          (str, _len) <- fdRead fromA (160 * 210 * 2 + 200)
+          putStrLn str
+          fdWrite toA "1,18\n" -- first action must be 1,1 to satrt game
 
-foreverPrint f = do (str, _len) <- fdRead f 64
-                    putStrLn str
-                    foreverPrint f
+          foreverPrint fromA toA
+
+foreverPrint fin fout = do (str, _len) <- fdRead fin (160 * 210 * 2 + 200)
+                           fdWrite fout "3,18\n"
+                           --x <- getChar
+                           putStrLn str
+                           putStrLn "####"
+                           foreverPrint fin fout
 
 -- June 11 - FIFO read from ALE works!
      -- run this
